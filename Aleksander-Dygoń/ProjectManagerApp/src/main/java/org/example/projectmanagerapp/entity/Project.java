f927707749569edbadf1493a34a6cb3b55b983f0
Sniezka <dygons14@gmail.com>
2025-03-08 16:29:11 +0100

    Configure env variables

--- Aleksander-Dygoń/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
+++ Aleksander-Dygoń/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
@@ -1,18 +1,15 @@
 package org.example.projectmanagerapp.entity;
 
-import jakarta.persistence.*;
-import lombok.*;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
-
-@Getter
-@Setter
 @Entity
-@NoArgsConstructor
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    public String name;
+    private String name;
+    private String description;
 }
--- Aleksander-Dygoń/ProjectManagerApp/src/test/java/org/example/projectmanagerapp/ProjectManagerAppApplicationTests.java
+++ Aleksander-Dygoń/ProjectManagerApp/src/test/java/org/example/projectmanagerapp/ProjectManagerAppApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.projectmanagerapp;
+package org.example.projectmanagerapp;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
