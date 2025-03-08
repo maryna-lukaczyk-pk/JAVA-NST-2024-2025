23807dc80dde7d8ea62558bf82ca579e2e105bb3
Indeed <gravitybartek@gmail.com>
2025-03-08 15:05:37 +0100

    add entity and repository

--- BARTOSZ-MISIEK/ProjectManagerApp/src/main/java/com/example/projectmanagerapp/ProjectManagerAppApplication.java
+++ BARTOSZ-MISIEK/ProjectManagerApp/src/main/java/com/example/projectmanagerapp/ProjectManagerAppApplication.java
@@ -2,7 +2,11 @@ package com.example.projectmanagerapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EntityScan("org.example.projectmanager.entity")
+@EnableJpaRepositories
 @SpringBootApplication
 public class ProjectManagerAppApplication {
 
