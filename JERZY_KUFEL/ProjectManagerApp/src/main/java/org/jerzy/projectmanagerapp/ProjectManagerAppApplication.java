9ec75b9fe6a33a44e71e1596a6d2b3a994901d39
Jerzy Kufel <jerzy.kufel@icloud.com>
2025-03-08 15:56:16 +0100

    Init project

---
+++ JERZY_KUFEL/ProjectManagerApp/src/main/java/org/jerzy/projectmanagerapp/ProjectManagerAppApplication.java
@@ -0,0 +1,13 @@
+package org.jerzy.projectmanagerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagerAppApplication.class, args);
+    }
+
+}
