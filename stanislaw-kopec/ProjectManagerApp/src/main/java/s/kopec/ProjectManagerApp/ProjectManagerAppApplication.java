0383278c4e16fd424576e7ffb22e3830817b57c0
Stach <stachu1325e@gmail.com>
2025-03-08 12:56:50 +0100

    spring init

---
+++ stanislaw-kopec/ProjectManagerApp/src/main/java/s/kopec/ProjectManagerApp/ProjectManagerAppApplication.java
@@ -0,0 +1,13 @@
+package s.kopec.ProjectManagerApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagerAppApplication.class, args);
+	}
+
+}
