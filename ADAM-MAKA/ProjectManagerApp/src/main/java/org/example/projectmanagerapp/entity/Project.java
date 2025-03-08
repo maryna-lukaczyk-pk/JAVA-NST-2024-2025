edb28ce752544bd876db2afb3f235a8a4139d74e
Adam <62599990+Adam-M-04@users.noreply.github.com>
2025-03-08 15:54:29 +0100

    project structure

---
+++ ADAM-MAKA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
@@ -0,0 +1,52 @@
+package org.example.projectmanagerapp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String description;
+
+    // Konstruktor bezargumentowy
+    public Project() {
+    }
+
+    // Konstruktor z argumentami
+    public Project(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    // Gettery i Settery
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
