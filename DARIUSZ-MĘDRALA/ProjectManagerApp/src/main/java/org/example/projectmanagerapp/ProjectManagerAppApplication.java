cf791e5d2972a0fba7125a9cbc9427f6bf71b17e
Dariusz Mędrala <dariusz.medrala@student.pk.edu.pl>
2025-03-09 16:36:31 +0100

    Enable JPA repositories and entity scanning in ProjectManagerApp
    Added @EntityScan and @EnableJpaRepositories annotations to the ProjectManagerAppApplication class to configure JPA entity scanning and repository management.

--- DARIUSZ-MĘDRALA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/ProjectManagerAppApplication.java
+++ DARIUSZ-MĘDRALA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/ProjectManagerAppApplication.java
@@ -2,7 +2,12 @@ package org.example.projectmanagerapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+
+@EntityScan
+@EnableJpaRepositories
 @SpringBootApplication
 public class ProjectManagerAppApplication {
 
