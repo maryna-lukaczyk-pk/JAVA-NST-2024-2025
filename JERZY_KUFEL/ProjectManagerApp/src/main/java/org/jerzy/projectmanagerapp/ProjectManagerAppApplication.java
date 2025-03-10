3cd74d646fb0badb8fb55880c163c2e48b20f9de
Jerzy Kufel <jerzy.kufel@icloud.com>
2025-03-10 19:11:07 +0100

    Init project

--- JERZY_KUFEL/ProjectManagerApp/src/main/java/org/jerzy/projectmanagerapp/ProjectManagerAppApplication.java
+++ JERZY_KUFEL/ProjectManagerApp/src/main/java/org/jerzy/projectmanagerapp/ProjectManagerAppApplication.java
@@ -2,8 +2,13 @@ package org.jerzy.projectmanagerapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication
+@EntityScan("org.jerzy.projectmanagerapp.entity")
+@EnableJpaRepositories("org.jerzy.projectmanagerapp.repository")
 public class ProjectManagerAppApplication {
 
     public static void main(String[] args) {
