8f3ba751073f5e9eebf921f145e88d748bf9f4b1
Stach <stachu1325e@gmail.com>
2025-03-11 19:22:16 +0100

    Update application.properties and ProjectManagerAppApplication.java, add Project.java and ProjectRepository.java

--- stanislaw-kopec/ProjectManagerApp/src/main/java/s/kopec/ProjectManagerApp/ProjectManagerAppApplication.java
+++ stanislaw-kopec/ProjectManagerApp/src/main/java/s/kopec/ProjectManagerApp/ProjectManagerAppApplication.java
@@ -2,8 +2,12 @@ package s.kopec.ProjectManagerApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan("s.kopec.ProjectManagerApp.entity")
+@EnableJpaRepositories("s.kopec.ProjectManagerApp.repository")
 public class ProjectManagerAppApplication {
 
 	public static void main(String[] args) {
