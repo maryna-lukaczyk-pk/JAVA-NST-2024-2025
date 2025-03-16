b4b7d46bc7328c4faa05b57d48cff354af14391e
Jakub Kapala <jakub.kapala@aptiv.com>
2025-03-16 18:35:59 +0100

    refactor: add constructor to `Project` entity

--- JAKUB-KAPALA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
+++ JAKUB-KAPALA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
@@ -27,4 +27,8 @@ public class Project {
 
     @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Task> tasks;
+
+    public Project(String name) {
+        this.name = name;
+    }
 }
