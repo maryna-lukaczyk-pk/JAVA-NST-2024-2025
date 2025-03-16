bcdaeae498caf9c41328f8b92e05e1f5df403be2
Aleksy Biela <aleks.302@wp.pl>
2025-03-16 20:34:26 +0100

    Added crud operations for Task and User

--- ALEKSY-BIELA/ProjectManagerApp/src/main/java/com/example/projectmanagerapp/controller/TasksController.java
+++ ALEKSY-BIELA/ProjectManagerApp/src/main/java/com/example/projectmanagerapp/controller/TasksController.java
@@ -1,9 +1,39 @@
 package com.example.projectmanagerapp.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.projectmanagerapp.entity.Project;
+import com.example.projectmanagerapp.entity.Tasks;
+import com.example.projectmanagerapp.repozytorium.ProjectRepository;
+import com.example.projectmanagerapp.repozytorium.TasksRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tasks")
 public class TasksController {
+    private final TasksRepository tasksRepository;
+
+    @Autowired
+    public TasksController(TasksRepository tasksRepository) {
+        this.tasksRepository = tasksRepository;
+    }
+
+    // Get project by id
+    @GetMapping("/{id}")
+    public ResponseEntity<Tasks> getTaskById(@PathVariable Long id) {
+        Optional<Tasks> task = tasksRepository.findById(id);
+        task.get().generatePriority();
+        return task.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    // Create a new project
+    @PostMapping
+    public ResponseEntity<Tasks> createProject(@RequestBody Tasks tasks) {
+        Tasks createdTasks = tasksRepository.save(tasks);
+        return new ResponseEntity<>(createdTasks, HttpStatus.CREATED);
+    }
 }
