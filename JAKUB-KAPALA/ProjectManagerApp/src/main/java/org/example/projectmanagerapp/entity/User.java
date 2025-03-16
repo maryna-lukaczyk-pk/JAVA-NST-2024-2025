eb55a1d2b899ef5eca535f2938b82f902e185ae4
Jakub Kapala <jakub.kapala@aptiv.com>
2025-03-16 17:18:32 +0100

    feat: add `Users.java` entity

---
+++ JAKUB-KAPALA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/User.java
@@ -0,0 +1,18 @@
+package org.example.projectmanagerapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+}
