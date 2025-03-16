0162285f0bbf6006a31df3d9e361d2260a11f513
badgersky <szymon.pt4sznik@gmail.com>
2025-03-16 15:54:23 +0100

    Classes for different priority levels

---
+++ SZYMON-PTASZNIK/ProjectManagerApp/src/main/java/com/example/projectmanagerapp/entity/MidPriority.java
@@ -0,0 +1,8 @@
+package com.example.projectmanagerapp.entity;
+
+public class MidPriority implements PriorityLevel {
+    @Override
+    public String getPriority() {
+        return "Medium";
+    }
+}
