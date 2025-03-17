b3eccd0f279cb8a6339958b1a4cd2c3d0137473b
Michal Kowal <michal.kowal.66@gmail.com>
2025-03-17 19:12:30 +0100

    Add ProjectController with simple get and post endpoints
    Add repository field, include in ctor. Add endpoints to get all projects and post a project

---
+++ MICHAL-KOWAL/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/ProjectController.java
@@ -0,0 +1,28 @@
+package org.example.projectmanagerapp.controller;
+
+import org.example.projectmanagerapp.entity.Project;
+import org.example.projectmanagerapp.entity.User;
+import org.example.projectmanagerapp.repository.ProjectRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/projects")
+public class ProjectController {
+    ProjectRepository projectRepository;
+
+    public ProjectController(ProjectRepository projectRepository) {
+        this.projectRepository = projectRepository;
+    }
+
+    @GetMapping("/all")
+    List<Project> all() {
+        return projectRepository.findAll();
+    }
+
+    @PostMapping("/new")
+    Project newProject(@RequestBody Project newProject) {
+        return projectRepository.save(newProject);
+    }
+}
