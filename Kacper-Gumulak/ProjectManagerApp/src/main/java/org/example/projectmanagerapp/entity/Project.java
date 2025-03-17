9b956ce638de2158adaeb2391f2306b0c768bd77
Kacper <kacper.gumulak@gmail.com>
2025-03-17 22:34:17 +0100

    Update database credentials and enhance Project entity with relationships to User and Task

--- Kacper-Gumulak/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
+++ Kacper-Gumulak/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
@@ -1,5 +1,6 @@
 package org.example.projectmanagerapp.entity;
 
+import java.util.Set;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,40 @@ import lombok.Setter;
 @Getter
 @Setter
 @NoArgsConstructor
+
+@Table(name = "projects")
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
+
+    @Column(nullable = false)
     private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "project_users",
+            joinColumns = @JoinColumn(name = "project_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> users;
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    private Set<Task> tasks;
+
+    public Project(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
