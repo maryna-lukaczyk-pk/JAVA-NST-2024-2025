5c795aff26f495dcd5878a56d16fe7d1ee1ecc6b
Adam <adam.maka.tp@gmail.com>
2025-03-18 20:19:04 +0100

    Add user entity

---
+++ ADAM-MAKA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/User.java
@@ -0,0 +1,14 @@
+package org.example.projectmanagerapp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String username;
+}
