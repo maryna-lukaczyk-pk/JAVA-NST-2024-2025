8d249f7e23c3f90308ef06ea9a5653ba265356d6
Grzegorz Kotkowski <grzegorz.kotkowski@student.pk.edu.pl>
2025-03-18 20:04:51 +0100

    Dodanie repozytorium UserRepository, ProjectRepository i TaskRepository

--- GRZEGORZ-KOTKOWSKI/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/repository/UserRepository.java
+++ GRZEGORZ-KOTKOWSKI/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package org.example.projectmanagerapp.repository;
 
-import org.example.projectmanagerapp.entity.Project;
+import org.example.projectmanagerapp.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Long> {
+public interface UserRepository extends JpaRepository<Users, Long> {
 
 }
