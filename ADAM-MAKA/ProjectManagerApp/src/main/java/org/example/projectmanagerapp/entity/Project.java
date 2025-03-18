ee5a62c22ea77e5eae2e72a37ab822c01ee73d87
Adam <adam.maka.tp@gmail.com>
2025-03-18 20:23:01 +0100

    Add task entity

--- ADAM-MAKA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
+++ ADAM-MAKA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
@@ -4,12 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.CascadeType;
+import java.util.List;
 
 @Entity
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
+
     private String name;
-    private String description;
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL)
+    private List<Task> tasks;
 }
---
+++ ADAM-MAKA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Task.java
@@ -0,0 +1,29 @@
+package org.example.projectmanagerapp.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "task_type")
+    private TaskType taskType;
+
+    @ManyToOne
+    private Project project;
+}
