81a8345e9a3d5cba758835a2c37a7e1601e3f73f
Stach <stachu1325e@gmail.com>
2025-03-18 22:20:56 +0100

    Add interface PriorityLevel, add classes HighPriority, MediumPriority, LowPriority. Modify enum TaskType. Add method getPriority() in interface PriorityLevel and 3 classes which inherits from it and those classes override getPriority(). Enum now implement interface and have a constructor and use method getPriority() to return String value.

--- stanislaw-kopec/ProjectManagerApp/src/main/java/s/kopec/ProjectManagerApp/model/TaskType.java
+++ stanislaw-kopec/ProjectManagerApp/src/main/java/s/kopec/ProjectManagerApp/model/TaskType.java
@@ -1,7 +1,18 @@
 package s.kopec.ProjectManagerApp.model;
 
-public enum TaskType {
-    LOW_PRIORITY,
-    MEDIUM_PRIORITY,
-    HIGH_PRIORITY;
+import s.kopec.ProjectManagerApp.interfaces.PriorityLevel;
+
+public enum TaskType implements PriorityLevel {
+    LOW_PRIORITY(new LowPriority()),
+    MEDIUM_PRIORITY(new MediumPriority()),
+    HIGH_PRIORITY(new HighPriority());
+
+    private final PriorityLevel priorityLevel;
+
+    TaskType(PriorityLevel priorityLevel) {this.priorityLevel = priorityLevel;}
+
+    @Override
+    public String getPriority() {
+        return priorityLevel.getPriority();
+    }
 }
