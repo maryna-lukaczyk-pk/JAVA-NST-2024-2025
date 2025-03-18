f922de352477460ab51cce64914eeec3c9105c4c
Oskar Świątek <pan.holingo@gmail.com>
2025-03-18 11:34:23 +0100

    feat: add `UserRepository` for CRUD operations on User entity.

---
+++ Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.example.projectmanagerapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.example.projectmanagerapp.entity.user.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
