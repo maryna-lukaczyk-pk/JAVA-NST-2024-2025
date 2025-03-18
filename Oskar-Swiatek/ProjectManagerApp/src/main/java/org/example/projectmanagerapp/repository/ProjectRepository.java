7e9948f705186d1bd1555edeb801e6cd1a865b47
Oskar Świątek <pan.holingo@gmail.com>
2025-03-18 11:47:02 +0100

    refactor(repository): update `ProjectRepository` structure.

--- Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/repository/ProjectRepository.java
+++ Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/repository/ProjectRepository.java
@@ -1,7 +1,12 @@
 package org.example.projectmanagerapp.repository;
 
-import org.example.projectmanagerapp.entity.Project;
-import org.springframework.data.repository.CrudRepository;
+import org.example.projectmanagerapp.entity.project.Project;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface ProjectRepository extends CrudRepository<Project, Long> {
+import java.util.List;
+
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
+    List<Project> findByNameContaining(String name);
 }
