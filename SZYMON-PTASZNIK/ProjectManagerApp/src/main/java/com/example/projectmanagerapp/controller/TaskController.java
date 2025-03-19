2a26d8eefd48e5eb2c3e9a40dae7e4bb5c75f142
badgersky <szymon.pt4sznik@gmail.com>
2025-03-19 11:32:42 +0100

    added controller for user with get and post methods. Added @RestController to task controller

--- SZYMON-PTASZNIK/ProjectManagerApp/src/main/java/com/example/projectmanagerapp/controller/TaskController.java
+++ SZYMON-PTASZNIK/ProjectManagerApp/src/main/java/com/example/projectmanagerapp/controller/TaskController.java
@@ -5,9 +5,11 @@ import com.example.projectmanagerapp.repository.TaskRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@RestController
 public class TaskController {
     private final TaskRepository taskRepository;
     public TaskController(TaskRepository taskRepository) {
