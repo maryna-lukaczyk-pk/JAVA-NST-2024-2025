85a75c984ff2db6ea430287bf422ed37963acf33
DominikG14 <dgasiorek14@gmail.com>
2025-03-20 09:26:48 +0100

    # Added 3 repositories: - ProjectRepository - UserRepository - TaskRepository

--- DOMINIK-GASIOREK/ProjectManagerApp/src/main/java/com/example/projectmanagerapp/repository/TaskRepository.java
+++ DOMINIK-GASIOREK/ProjectManagerApp/src/main/java/com/example/projectmanagerapp/repository/TaskRepository.java
@@ -1,10 +1,10 @@
 package com.example.projectmanagerapp.repository;
 
-import com.example.projectmanagerapp.entity.Project;
+import com.example.projectmanagerapp.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProjectRepository extends JpaRepository<Project, Long> {
+public interface TaskRepository extends JpaRepository<Task, Long> {
 
 }
