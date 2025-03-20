76812616b2bc5ec5c5bd17e64a35022e4f556648
Mateusz Janicki <mj4nicki@gmail.com>
2025-03-20 21:44:30 +0100

    lab2- dodanie repository

---
+++ MATEUSZ-JANICKI/ProjectManagerApp/src/main/java/org/example/projectmanager/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package org.example.projectmanager.repository;
+
+import org.example.projectmanager.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
---
+++ MATEUSZ-JANICKI/ProjectManagerApp/src/main/java/org/example/projectmanager/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.example.projectmanager.repository;
+
+import org.example.projectmanager.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
