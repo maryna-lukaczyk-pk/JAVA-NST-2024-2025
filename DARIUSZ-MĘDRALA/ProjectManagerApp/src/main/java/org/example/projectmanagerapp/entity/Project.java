5beeec6928793ceb0ac809b62ba60234ac757509
Dariusz Mędrala <dariusz.medrala@student.pk.edu.pl>
2025-03-20 12:50:41 +0100

    Add Project, Task and User entities with relationships
    Added Porject, Task and User entities. Implemented relationshiops: Project & Users (Many-to-Many), Proejct & Tasks (One-to-Many), Task & Project (Many-to-One). Introduces enum for task types.

--- DARIUSZ-MĘDRALA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
+++ DARIUSZ-MĘDRALA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
@@ -1,18 +1,31 @@
 package org.example.projectmanagerapp.entity;
-
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-
+@Table(name = "projects")
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(columnDefinition = "VARCHAR(255")
     private String name;
+
+    @ManyToMany(cascade = { CascadeType.ALL})
+    @JoinTable(
+            name = "project_user",
+            joinColumns = { @JoinColumn(name = "project_id") },
+            inverseJoinColumns = { @JoinColumn(name = "user_id")}
+    )
+    private List<User> users;
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Task> tasks;
 }
