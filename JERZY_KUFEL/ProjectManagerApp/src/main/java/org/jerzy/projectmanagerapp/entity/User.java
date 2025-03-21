a108776f8f6ab1de12e4db9a745aa9af515e736b
Jerzy Kufel <jerzy.kufel@icloud.com>
2025-03-21 18:53:31 +0100

    Create project task and user entities

---
+++ JERZY_KUFEL/ProjectManagerApp/src/main/java/org/jerzy/projectmanagerapp/entity/User.java
@@ -0,0 +1,24 @@
+package org.jerzy.projectmanagerapp.entity.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "VARCHAR(255)")
+    private String username;
+
+    @ManyToMany(mappedBy = "users")
+    private List<Project> projects;
+}
