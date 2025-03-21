f2c1e2f72a1295cac1baa9bb24272c23f09cdcb6
Justyna <justynakaczkowska10@gmail.com>
2025-03-21 15:44:14 +0100

    Utworzenie encji oraz relacji, utworzenie priorytetow zadan, dodanie repozytoriow, utworzenie kontrolerow REST API

---
+++ JUSTYNA-KACZKOWSKA/ProjectManagerApp/src/main/java/com/example/projectmanagerapp/controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.example.projectmanagerapp.controllers;
+
+
+import com.example.projectmanagerapp.entity.User;
+import com.example.projectmanagerapp.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    private final UserRepository repository;
+
+    UserController(UserRepository repository) {
+        this.repository= repository;
+    }
+
+    @GetMapping("/users")
+    List<User> all() {
+        return repository.findAll();
+    }
+
+    @PostMapping("/users")
+    User newUser(@RequestBody User newUser) {
+        return repository.save(newUser);
+    }
+}
