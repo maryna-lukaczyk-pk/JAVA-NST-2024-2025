1116dfff88f3b669c9615f2083ba6899972fca44
Michal Dziwik <mdziwikk@gmail.com>
2025-03-21 20:45:25 +0100

    dodanie encji, repo oraz controlerow

--- MICHAL-DZIWIK/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
+++ MICHAL-DZIWIK/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
@@ -4,17 +4,26 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
+@Table(name = "projects")
 @Getter
 @Setter
 @NoArgsConstructor
-
 public class Project {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, length = 255)
     private String name;
 
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<ProjectUser> projectUsers = new HashSet<>();
+
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Task> tasks = new HashSet<>();
 }
