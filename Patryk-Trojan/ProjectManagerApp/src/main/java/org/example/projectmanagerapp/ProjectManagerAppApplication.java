3d9bacfdefa206318e5b8c71f5944c3df2574bc3
patryktr <patryk.trojan@onet.pl>
2025-03-22 16:49:47 +0100

    Lab2 completed- Complete lab with entity creation, repositories, and REST API

--- Patryk-Trojan/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/ProjectManagerAppApplication.java
+++ Patryk-Trojan/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/ProjectManagerAppApplication.java
@@ -2,10 +2,8 @@ package org.example.projectmanagerapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-
-
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
