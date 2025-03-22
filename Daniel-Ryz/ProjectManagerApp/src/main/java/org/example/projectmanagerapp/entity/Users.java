1e9c6c8ab487bca2e9d651d0943afd96c4df5537
Daniel <danielryz4321@gmail.com>
2025-03-22 10:06:43 +0100

    ADD: Dodanie i wstępna konfiguracja encji Users.

---
+++ Daniel-Ryz/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Users.java
@@ -0,0 +1,20 @@
+package org.example.projectmanagerapp.entity;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @ManyToMany(mappedBy = "users")
+    private Set<Project> projects = new HashSet<>();
+
+
+}
