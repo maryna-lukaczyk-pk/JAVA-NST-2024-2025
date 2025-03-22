8fd51393090ae7e92a56f1755fba9bec677ff9ff
Daniel <danielryz4321@gmail.com>
2025-03-22 10:09:03 +0100

    ADD: Dodanie do Project.java relacji ManyToMany i OneToMany.

--- Daniel-Ryz/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
+++ Daniel-Ryz/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
@@ -4,6 +4,12 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
+import org.springframework.scheduling.config.Task;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -13,5 +19,18 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @ManyToMany
+    @JoinTable(
+            name = "project_users",
+            joinColumns = @JoinColumn(name = "project_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> users = new HashSet<>();
+
+    @OneToMany(mappedBy = "project")
+    private List<Task> tasks = new ArrayList<>();
 }
