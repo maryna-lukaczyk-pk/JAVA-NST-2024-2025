881599556203d7228199a66e391de3316b77ec0a
Szymon <szymon2206033@gmail.com>
2025-03-22 21:07:24 +0100

    Create entity classes

---
+++ Szymon-Basta/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Projects.java
@@ -0,0 +1,24 @@
+package org.example.projectmanagerapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+
+@Table(name = "projects")
+public class Projects {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "project")
+    private List<Tasks> tasks;
+}
---
+++ Szymon-Basta/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/repository/ProjectRepository.java
@@ -0,0 +1,6 @@
+package org.example.projectmanagerapp.repository;
+
+import org.example.projectmanagerapp.entity.Projects;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProjectRepository extends JpaRepository<Projects, Long> { }
