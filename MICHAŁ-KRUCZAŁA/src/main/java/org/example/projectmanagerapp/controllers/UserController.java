c57fe771e8afca1488e9ff5bb53f4040494680b8
Michał Kruczała <kruczalamichal@gmail.com>
2025-03-22 15:22:09 +0100

    Added controllers for each entity

---
+++ MICHAŁ-KRUCZAŁA/src/main/java/org/example/projectmanagerapp/controllers/UserController.java
@@ -0,0 +1,29 @@
+package org.example.projectmanagerapp.controllers;
+
+import org.example.projectmanagerapp.entity.User;
+import org.example.projectmanagerapp.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = userRepository.save(user);
+        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+    }
+}
