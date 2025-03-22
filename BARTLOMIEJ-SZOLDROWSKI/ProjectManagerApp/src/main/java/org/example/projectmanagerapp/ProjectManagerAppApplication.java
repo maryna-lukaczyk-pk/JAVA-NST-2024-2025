9cf1a46218a6ad536bc9c90d99b3e388c4ac5156
Bartlomiej <barteks8101@gmail.com>
2025-03-22 14:59:01 +0100

    Update: table created, file .idea removed

--- BARTLOMIEJ-SZOLDROWSKI/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/ProjectManagerAppApplication.java
+++ BARTLOMIEJ-SZOLDROWSKI/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/ProjectManagerAppApplication.java
@@ -1,4 +1,4 @@
-package com.example.projectmanagerapp;
+package org.example.projectmanagerapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan("com.furtak.ProjectManagerApp.entity")
-@EnableJpaRepositories("com.furtak.ProjectManagerApp.repository")
+@EntityScan("org.example.projectmanagerapp.entity")
+@EnableJpaRepositories("org.example.projectmanagerapp.repository")
 public class ProjectManagerAppApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(ProjectManagerAppApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagerAppApplication.class, args);
+	}
 }
--- BARTLOMIEJ-SZOLDROWSKI/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
+++ BARTLOMIEJ-SZOLDROWSKI/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
@@ -1,17 +1,23 @@
 package org.example.projectmanagerapp.entity;
 
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@NoArgsConstructor
 @Getter
 @Setter
-@NoArgsConstructor
 public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private long id;
+
     private String name;
+
 }
