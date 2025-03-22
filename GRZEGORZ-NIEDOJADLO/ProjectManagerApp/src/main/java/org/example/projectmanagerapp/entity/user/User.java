a47a26397777ef86d1d01b387e73043a3bdb5117
Grzegorz <grzegorz30017@gmail.com>
2025-03-22 23:54:07 +0100

    Entity User added

---
+++ GRZEGORZ-NIEDOJADLO/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/user/User.java
@@ -0,0 +1,17 @@
+package org.example.projectmanagerapp.entity.user;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
