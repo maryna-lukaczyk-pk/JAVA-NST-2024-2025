6eba1c52aa1c53ea1166d590618a538a8994d9fb
Michał Kruczała <kruczalamichal@gmail.com>
2025-03-22 13:34:54 +0100

    Added entities + relations

---
+++ MICHAŁ-KRUCZAŁA/src/main/java/org/example/projectmanagerapp/entity/Task.java
@@ -0,0 +1,31 @@
+package org.example.projectmanagerapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "tasks")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String title;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "task_type")
+    private Task_Type task_type;
+    @ManyToOne
+    private Project project;
+
+    private enum Task_Type {
+        TYPE1, TYPE2, TYPE3
+    }
+}
---
+++ MICHAŁ-KRUCZAŁA/src/main/java/org/example/projectmanagerapp/entity/User.java
@@ -0,0 +1,25 @@
+package org.example.projectmanagerapp.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    @ManyToMany(mappedBy = "users")
+    private Set<Project> projects;
+}
