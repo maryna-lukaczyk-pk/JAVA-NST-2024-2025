575c3152efdf6965dd40b63fee96155582891989
Bartlomiej Fryc <bartlomiej.fryc.7@gmail.com>
2025-03-23 20:37:15 +0100

    Utworzenie kontrolerów REST API

--- BARTLOMIEJ FRYC/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/repository/UserRepository.java
+++ BARTLOMIEJ FRYC/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package org.example.projectmanagerapp.repository;
 
-import org.example.projectmanagerapp.entity.Task;
+import org.example.projectmanagerapp.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<Task, Long> {
+public interface UserRepository extends JpaRepository<Users, Long> {
 }
