16e20133f765787271f61965a1a5866fbecdfb55
rafalpaliwoda <r.paliwoda1928@gmail.com>
2025-03-23 10:09:19 +0100

    feat: implement usercontroller

--- Rafał-Paliwoda/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controllers/UserController.java
+++ Rafał-Paliwoda/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controllers/UserController.java
@@ -1,18 +1,29 @@
 package org.example.projectmanagerapp.controllers;
 
 import org.example.projectmanagerapp.entity.User;
+import org.example.projectmanagerapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/user")
 public class UserController {
-    @GetMapping()
-    public User getUser() {
-        User user = new User();
-        user.setId(1L);
-        user.setUsername("admin");
-        return user;
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
