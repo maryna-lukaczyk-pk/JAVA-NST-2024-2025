3d770687346bfc619ba6b7eb15c3edce067f9fbb
Grzegorz <grzegorz30017@gmail.com>
2025-03-23 11:03:04 +0100

    Add Repository

--- GRZEGORZ-NIEDOJADLO/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/repository/ProjectRepository.java
+++ GRZEGORZ-NIEDOJADLO/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/repository/ProjectRepository.java
@@ -2,8 +2,6 @@ package org.example.projectmanagerapp.repository;
 
 import org.example.projectmanagerapp.entity.project.Project;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 }
