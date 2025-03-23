46102929b613f147043115ed069619843a670248
Bartosz Walczyk <walczykbartosz03@gmail.com>
2025-03-23 18:35:17 +0100

    added user and task repositories and updated project repository

--- Bartosz-Walczyk/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/repository/ProjectRepository.java
+++ Bartosz-Walczyk/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/repository/ProjectRepository.java
@@ -1,7 +1,7 @@
 package org.example.projectmanagerapp.repository;
 
 import org.example.projectmanagerapp.entity.Project;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProjectRepository extends CrudRepository<Project, Long> {
+public interface ProjectRepository extends JpaRepository<Project, Long> {
 }
