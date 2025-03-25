47ff830c9276e23aa49d5fb2679394ead1df1fe7
Kamil <kamil.wrona@student.pk.edu.pl>
2025-03-25 08:39:58 +0100

    feat: implement core entities, priority system and REST controllers
    - Created all required entities (User, Project, Task) with proper JPA relationships
    
    - Implemented dynamic task priority system using polymorphism:
    
      * Added PriorityLevel interface with getPriority() method
    
      * Created HighPriority, MediumPriority and LowPriority implementations
    
    - Added JPA repositories for all entities:
    
      * UserRepository
    
      * ProjectRepository
    
      * TaskRepository
    
    - Implemented basic REST controllers with CRUD operations:
    
      * Added GET endpoints for all entities
    
      * Added POST endpoints for creating new entities
    
    - Ensured proper database schema generation via Hibernate

---
+++ KAMIL-WRONA/ProjectManagerApp/src/main/java/com/example/User.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+}
