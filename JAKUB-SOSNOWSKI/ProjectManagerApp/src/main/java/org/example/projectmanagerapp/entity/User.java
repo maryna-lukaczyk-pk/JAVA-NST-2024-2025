8267db9d9c58c3d8c6bf7061899d648858098f44
Jakub <JakubSosnowski737@gmail.com>
2025-03-26 18:35:30 +0100

    Created eneity, repository, and controller folders. Moved relevant classes into the new directories. Updated package declarations

---
+++ JAKUB-SOSNOWSKI/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/User.java
@@ -0,0 +1,38 @@
+package org.example.projectmanagerapp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    public User() {
+    }
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
