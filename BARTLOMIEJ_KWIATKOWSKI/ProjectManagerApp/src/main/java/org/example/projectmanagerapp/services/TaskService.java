863a57ad147a0de098cc42c12b66e3399dbbb3db
BartlomiejKwiatkowsk <bartekkw6@gmail.com>
2025-04-27 17:46:40 +0200

    Dodanie TaskService.java, ProjectService.java, UserService.java

---
+++ BARTLOMIEJ_KWIATKOWSKI/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/services/TaskService.java
@@ -0,0 +1,22 @@
+package org.example.projectmanagerapp.service;
+
+import org.example.projectmanagerapp.entity.Tasks;
+import org.example.projectmanagerapp.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Tasks> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Tasks createTask(Tasks task) {
+        return taskRepository.save(task);
+    }
+}
---
+++ BARTLOMIEJ_KWIATKOWSKI/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/services/UserService.java
@@ -0,0 +1,22 @@
+package org.example.projectmanagerapp.service;
+
+import org.example.projectmanagerapp.entity.Users;
+import org.example.projectmanagerapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<Users> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Users createUser(Users user) {
+        return userRepository.save(user);
+    }
+}
