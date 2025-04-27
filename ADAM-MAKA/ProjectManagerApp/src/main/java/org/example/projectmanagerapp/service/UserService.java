94ccee0c9bb823577aaab43211f2c216a5caf53d
Adam <adam.maka.tp@gmail.com>
2025-04-27 17:22:36 +0200

    Create service classes for User, Project, and Task management

---
+++ ADAM-MAKA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/service/UserService.java
@@ -0,0 +1,26 @@
+package org.example.projectmanagerapp.service;
+
+import org.example.projectmanagerapp.entity.User;
+import org.example.projectmanagerapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+}
