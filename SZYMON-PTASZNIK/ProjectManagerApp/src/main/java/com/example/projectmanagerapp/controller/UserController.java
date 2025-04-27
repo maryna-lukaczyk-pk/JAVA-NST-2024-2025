95ca0110b86f7f4a269c35c24c11046e3c0e366e
badgersky <szymon.pt4sznik@gmail.com>
2025-04-27 10:02:06 +0200

    added needed import to user controller file

--- SZYMON-PTASZNIK/ProjectManagerApp/src/main/java/com/example/projectmanagerapp/controller/UserController.java
+++ SZYMON-PTASZNIK/ProjectManagerApp/src/main/java/com/example/projectmanagerapp/controller/UserController.java
@@ -4,11 +4,14 @@ import com.example.projectmanagerapp.entity.User;
 import com.example.projectmanagerapp.services.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
