a9b31a144294d24ed385caa1da941ff1572e2eb0
Michal Kowal <michal.kowal.66@gmail.com>
2025-05-01 18:26:40 +0200

    User controller add swagger annotations
    Add missing Tag and Operation annotations for controller class and methods

--- MICHAL-KOWAL/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/UserController.java
+++ MICHAL-KOWAL/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/UserController.java
@@ -1,11 +1,14 @@
 package org.example.projectmanagerapp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.projectmanagerapp.entity.User;
 import org.example.projectmanagerapp.repository.UserRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "User", description = "User controller")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -16,12 +19,15 @@ public class UserController {
     }
 
     @GetMapping("/all")
+    @Operation(summary = "Get all users", description = "Gets all users")
     List<User> all() {
         return userRepository.findAll();
     }
 
     @PostMapping("/")
-    User newUser(@RequestBody User newUser) {
+    @Operation(summary = "Create user", description = "Creates a new user")
+    User newUser(@RequestBody User newUser)
+    {
         return userRepository.save(newUser);
     }
 }
