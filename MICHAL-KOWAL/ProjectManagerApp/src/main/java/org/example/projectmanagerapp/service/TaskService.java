84c9883f202370b8306867e0ba3fe714239a8d60
Michal Kowal <michal.kowal.66@gmail.com>
2025-05-01 20:10:17 +0200

    Task service add get by id, update and delete methods

--- MICHAL-KOWAL/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/service/TaskService.java
+++ MICHAL-KOWAL/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/service/TaskService.java
@@ -18,7 +18,30 @@ public class TaskService {
         return taskRepository.findAll();
     }
 
+    public Task getTaskById(Long id) {
+        return taskRepository.findById(id)
+                             .orElseThrow(() -> new RuntimeException("Task does not exist"));
+    }
+
     public Task createTask(Task task) {
         return taskRepository.save(task);
+    }
+
+    public Task updateById(Long id, Task task) {
+        if (!taskRepository.existsById(id)) {
+            throw new RuntimeException("Task with id " + id + " does not exist");
+        }
+
+        task.setId(id);
+        return taskRepository.save(task);
+    }
+
+    public Boolean deleteById(Long id) {
+        if (!taskRepository.existsById(id)) {
+            return false;
+        }
+
+        taskRepository.deleteById(id);
+        return true;
     }
 }
