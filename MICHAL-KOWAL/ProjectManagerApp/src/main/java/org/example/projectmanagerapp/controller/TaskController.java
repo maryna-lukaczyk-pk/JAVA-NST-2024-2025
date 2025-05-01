831dd2fd48fddb83ce025ac050fc1c0f5ad9c0fb
Michal Kowal <michal.kowal.66@gmail.com>
2025-05-01 18:27:17 +0200

    Task controller add swagger annotations
    Add missing Tag and Operation annotations for controller class and methods

--- MICHAL-KOWAL/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/TaskController.java
+++ MICHAL-KOWAL/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/TaskController.java
@@ -1,11 +1,14 @@
 package org.example.projectmanagerapp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.projectmanagerapp.entity.Task;
 import org.example.projectmanagerapp.repository.TaskRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Tag(name = "Task", description = "Task controller")
 @RestController
 @RequestMapping("/api/tasks")
 public class TaskController {
@@ -16,12 +19,15 @@ public class TaskController {
     }
 
     @GetMapping("/all")
+    @Operation(summary = "Get all tasks", description = "Gets all tasks")
     List<Task> all() {
         return taskRepository.findAll();
     }
 
     @PostMapping("/")
-    Task newTask(@RequestBody Task newTask) {
+    @Operation(summary = "Create task", description = "Creates a new task")
+    Task newTask(@RequestBody Task newTask)
+    {
         return taskRepository.save(newTask);
     }
 }
