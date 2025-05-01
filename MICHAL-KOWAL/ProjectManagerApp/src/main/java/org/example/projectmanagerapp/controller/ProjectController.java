8c503dc5d4a6b0968eac12149cf841a7ff4cc3e5
Michal Kowal <michal.kowal.66@gmail.com>
2025-05-01 20:16:03 +0200

    Project controller add methods for update, delete and get by id
    Add methods using respectively PUT, DELETE, GET HTTP methods, update swagger annotation

--- MICHAL-KOWAL/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/ProjectController.java
+++ MICHAL-KOWAL/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/ProjectController.java
@@ -1,9 +1,12 @@
 package org.example.projectmanagerapp.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.projectmanagerapp.entity.Project;
 import org.example.projectmanagerapp.service.ProjectService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,5 +30,47 @@ public class ProjectController {
     Project newProject(@RequestBody Project newProject)
     {
         return projectService.createProject(newProject);
+    }
+
+    @PutMapping("/update/{id}")
+    @Operation(summary = "Update project", description = "Updates project by id")
+    @Parameter(name =  "id", description  = "id of the project to update", required = true)
+    ResponseEntity<Project> updateProject(@PathVariable Long id, @RequestBody Project newProject)
+    {
+        Project updatedProject;
+        try {
+            updatedProject = projectService.updateById(id, newProject);
+        }
+        catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(updatedProject, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @Operation(summary = "Delete project", description = "Deletes project by id")
+    @Parameter(name = "id", description  = "id of the project to delete", required = true)
+    ResponseEntity<Void> deleteProject(@PathVariable Long id) {
+        if (!projectService.deleteById(id)) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Get project", description = "Gets a project by id")
+    @Parameter(name = "id", description  = "id of the project", required = true)
+    ResponseEntity<Project> getProjectById(@PathVariable Long id) {
+        Project project;
+        try {
+            project = projectService.getProjectById(id);
+        }
+        catch (RuntimeException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(project, HttpStatus.OK);
     }
 }
