06a55fc4c1b5f6ba46bb6312d66d09f0a7b5782b
badgersky <szymon.pt4sznik@gmail.com>
2025-05-04 16:35:49 +0200

    updated project controller

--- SZYMON-PTASZNIK/ProjectManagerApp/src/main/java/com/example/projectmanagerapp/controller/ProjectController.java
+++ SZYMON-PTASZNIK/ProjectManagerApp/src/main/java/com/example/projectmanagerapp/controller/ProjectController.java
@@ -1,35 +1,49 @@
 package com.example.projectmanagerapp.controller;
 
 import com.example.projectmanagerapp.entity.Project;
-import com.example.projectmanagerapp.repository.ProjectRepository;
+import com.example.projectmanagerapp.services.ProjectService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
 @RestController
 @Tag(name="Project", description="Opeartions for managing projects")
 public class ProjectController {
-    private final ProjectRepository projectRepository;
+    private final ProjectService projectService;
 
-    ProjectController(ProjectRepository projectRepository) {
-        this.projectRepository = projectRepository;
+    ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
     }
 
     @GetMapping("/projects")
     @Operation(summary="List all projects", description="Returns list of all projects")
-    List<Project> getProjects() {
-        return projectRepository.findAll();
+    public List<Project> getProjects() {
+        return projectService.getAllProjects();
     }
 
     @PostMapping("/projects")
     @Operation(summary="Add new project", description="Creates new project")
-    Project addProject(@RequestBody Project project) {
-        return projectRepository.save(project);
+    public Project createProject(@RequestBody Project project) {return projectService.createProject(project);
+    }
+
+    @PutMapping("projects/{id}")
+    @Operation(summary = "Update an existing project", description = "Updates the details of an existing project by ID")
+    public Project updateProject(@PathVariable Long id, @RequestBody Project project) {
+        return projectService.updateProject(id, project);
+    }
+
+    @DeleteMapping("projects/{id}")
+    @Operation(summary = "Delete a project", description = "Deletes the project by ID")
+    public void deleteProject(@PathVariable Long id) {
+        projectService.deleteProject(id);
     }
 }
