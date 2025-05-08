ce3be5c267ab6200446551883d5ccedfa037b3a5
Szymon <szymon2206033@gmail.com>
2025-05-08 22:16:55 +0200

    Add TaskService and fixed task display (recursion)

--- Szymon-Basta/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/TaskController.java
+++ Szymon-Basta/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/TaskController.java
@@ -4,8 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.projectmanagerapp.entity.Tasks;
-import org.example.projectmanagerapp.repository.TaskRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.example.projectmanagerapp.service.TaskService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,22 +14,23 @@ import java.util.List;
 @Tag(name = "Tasks", description = "Operations for managing tasks")
 public class TaskController {
 
-    @Autowired
-    private TaskRepository taskRepository;
+    private final TaskService taskService;
 
-    // GET
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
     @Operation(summary = "Retrieve all tasks", description = "Returns a list of all tasks from the database")
     @GetMapping
     public List<Tasks> getAllTasks() {
-        return taskRepository.findAll();
+        return taskService.getAllTasks();
     }
 
-    // POST
     @Operation(summary = "Create a new task", description = "Creates a task with the provided information")
     @PostMapping
     public Tasks createTask(
             @Parameter(description = "Task object to be created")
             @RequestBody Tasks task) {
-        return taskRepository.save(task);
+        return taskService.createTask(task);
     }
 }
