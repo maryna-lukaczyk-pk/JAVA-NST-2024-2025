ac04688286acaf132762a5cc2982798da2623fe6
Indeed <gravitybartek@gmail.com>
2025-05-11 12:44:26 +0200

    add missing properties to dto and equality

--- BARTOSZ-MISIEK/ProjectManagerApp/src/main/java/org/example/projectmanager/dto/user/UserDto.java
+++ BARTOSZ-MISIEK/ProjectManagerApp/src/main/java/org/example/projectmanager/dto/user/UserDto.java
@@ -2,6 +2,8 @@ package org.example.projectmanager.dto.user;
 
 import org.example.projectmanager.entity.user.User;
 
+import java.util.Objects;
+
 public class UserDto {
     public Long id;
     public String username;
@@ -11,5 +13,16 @@ public class UserDto {
         self.id = user.getId();
         self.username = user.getUsername();
         return self;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof UserDto userDto)) return false;
+        return Objects.equals(id, userDto.id) && Objects.equals(username, userDto.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, username);
     }
 }
