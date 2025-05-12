858e6f568d53f7afb8881f07a5bb27f6d4bf7d3a
Oskar Świątek <pan.holingo@gmail.com>
2025-05-12 15:18:14 +0200

    feat: add `UserServie` with basic CRUD operations

---
+++ Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/service/UserService.java
@@ -0,0 +1,39 @@
+package org.example.projectmanagerapp.service;
+
+import org.example.projectmanagerapp.entity.user.User;
+import org.example.projectmanagerapp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(Long id, User userDetails) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        user.setUsername(userDetails.getUsername());
+        return userRepository.save(user);
+    }
+
+    public User deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
