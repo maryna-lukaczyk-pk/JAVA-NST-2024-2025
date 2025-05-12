7c9ed52b9657b71716b09c51f4f0683fda618dfa
Oskar Świątek <pan.holingo@gmail.com>
2025-05-12 15:46:30 +0200

    refactor: update `TaskController` to use `TaskService` instead of repository.

--- Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/TaskController.java
+++ Oskar-Swiatek/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/TaskController.java
@@ -1,8 +1,7 @@
 package org.example.projectmanagerapp.controller;
 
 import org.example.projectmanagerapp.entity.task.Task;
-import org.example.projectmanagerapp.repository.TaskRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.example.projectmanagerapp.service.TaskService;
 import org.springframework.web.bind.annotation.*;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -16,43 +15,42 @@ import java.util.List;
 @RequestMapping("/api/tasks")
 public class TaskController {
 
-    @Autowired
-    private TaskRepository taskRepository;
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
 
     @Operation(summary = "Retrieve all tasks")
     @GetMapping
     public List<Task> getAllTasks() {
-        return taskRepository.findAll();
+        return taskService.getAllTasks();
     }
 
     @Operation(summary = "Retrieve a task by ID")
     @GetMapping("/{id}")
     @Parameter(description = "ID of the task to retrieve")
     public Task getTaskById(@PathVariable Long id) {
-        return taskRepository.findById(id).orElseThrow(() -> new RuntimeException("Task not found"));
+        return taskService.getTaskById(id);
     }
 
     @Operation(summary = "Create a new task")
     @PostMapping
     public Task createTask(@RequestBody Task task) {
-        return taskRepository.save(task);
+        return taskService.createTask(task);
     }
 
     @Operation(summary = "Update an existing task")
     @PutMapping("/{id}")
     @Parameter(description = "ID of the task to update")
     public Task updateTask(@PathVariable Long id, @RequestBody Task taskDetails) {
-        Task task = taskRepository.findById(id).orElseThrow(() -> new RuntimeException("Task not found"));
-        task.setTitle(taskDetails.getTitle());
-        task.setDescription(taskDetails.getDescription());
-        task.setTaskType(taskDetails.getTaskType());
-        return taskRepository.save(task);
+        return taskService.updateTask(id, taskDetails);
     }
 
     @Operation(summary = "Delete a task by ID")
     @DeleteMapping("/{id}")
     @Parameter(description = "ID of the task to delete")
     public void deleteTask(@PathVariable Long id) {
-        taskRepository.deleteById(id);
+        taskService.deleteTask(id);
     }
 }
