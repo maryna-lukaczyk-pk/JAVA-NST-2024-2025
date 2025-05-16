d916091e07cfb6047ca4e9fd3fb4e7079ef151d5
Kamil <kamil.wrona@student.pk.edu.pl>
2025-05-16 12:14:21 +0200

    Add Jacoco Maven profile for integration test coverage reporting

--- KAMIL-WRONA/ProjectManagerApp/src/test/java/com/example/ProjectManagerAppApplicationTests.java
+++ KAMIL-WRONA/ProjectManagerApp/src/test/java/com/example/ProjectManagerAppApplicationTests.java
@@ -3,7 +3,7 @@ package org.example.projectmanagerapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = ProjectManagerAppApplication.class)
 class ProjectManagerAppApplicationTests {
 
     @Test
