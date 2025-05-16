9f9236a802497f6fffc2420247eb5eab3570c240
Stach <stachu1325e@gmail.com>
2025-05-16 14:09:09 +0200

    Add test profile in pom.xml

--- stanislaw-kopec/ProjectManagerApp/src/test/java/s/kopec/ProjectManagerApp/CucumberTest.java
+++ stanislaw-kopec/ProjectManagerApp/src/test/java/s/kopec/ProjectManagerApp/CucumberTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty", "summary"},
+        plugin = {"pretty", "summary", "html:target/cucumber-reports.html"},
         features = "src/test/resources/features",
         glue = "s.kopec.ProjectManagerApp.steps"
 )
