342379ceb00438c6511467f86ae05dc675a06c2a
Patrycja <patrycja.nowakowska3@gmail.com>
2025-05-19 23:47:26 +0200

    Zmiany w encji Project oraz aktualizacja serwisów i testów

--- PATRYCJA-NOWAKOWSKA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
+++ PATRYCJA-NOWAKOWSKA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/entity/Project.java
@@ -17,7 +17,10 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
+    private String description;
+
     @ManyToMany
     @JoinTable(
             name = "project_users",
