40fdef22e1fb9a7f129b36b6ba4f9f7578ea8955
Patrycja <patrycja.nowakowska3@gmail.com>
2025-05-23 16:43:48 +0200

    Dodanie opisu kontrolerów dla Project

--- PATRYCJA-NOWAKOWSKA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/ProjectController.java
+++ PATRYCJA-NOWAKOWSKA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/ProjectController.java
@@ -1,6 +1,9 @@
 package org.example.projectmanagerapp.controller;
 
 import org.example.projectmanagerapp.entity.Project;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.projectmanagerapp.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -9,17 +12,20 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/projects")
+@Tag(name = "Projects", description = "Operacje na projektach")
 public class ProjectController {
 
     @Autowired
     private ProjectRepository projectRepository;
 
     @GetMapping
+    @Operation (summary = "Pobierz wszystkie projekty", description = "Zwraca liste wszystkich zapisanych projektów")
     public List<Project> getAllProjects() {
         return projectRepository.findAll();
     }
 
     @PostMapping
+    @Operation (summary = "Utwórz nowy projekt", description = "Dodaje nowy projekt")
     public Project createProject(@RequestBody Project project) {
         return projectRepository.save(project);
     }
