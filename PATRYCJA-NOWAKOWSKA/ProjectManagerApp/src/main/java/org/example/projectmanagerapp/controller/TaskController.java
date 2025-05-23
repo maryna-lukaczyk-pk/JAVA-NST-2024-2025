a3e144f6428803cf080e2ded9783c6b47ead5fb7
Patrycja <patrycja.nowakowska3@gmail.com>
2025-05-23 17:53:03 +0200

    Utworzono klasę serwisową oraz zmodyfikowano kontroler Task

--- PATRYCJA-NOWAKOWSKA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/TaskController.java
+++ PATRYCJA-NOWAKOWSKA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/TaskController.java
@@ -3,7 +3,7 @@ package org.example.projectmanagerapp.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.projectmanagerapp.entity.Task;
-import org.example.projectmanagerapp.repository.TaskRepository;
+import org.example.projectmanagerapp.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,18 +14,22 @@ import java.util.List;
 @Tag(name = "Tasks", description = "Operacje związane z zadaniami")
 public class TaskController {
 
+    private final TaskService taskService;
+
     @Autowired
-    private TaskRepository taskRepository;
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
 
     @GetMapping
     @Operation(summary = "Pobierz listę wszystkich zadań", description = "Zwraca listę wszystkich zadań")
     public List<Task> getAllTasks() {
-        return taskRepository.findAll();
+        return taskService.getAllTasks();
     }
 
     @PostMapping
     @Operation(summary = "Dodaj nowe zadanie", description = "Tworzy nowe zadanie")
     public Task createTask(@RequestBody Task task) {
-        return taskRepository.save(task);
+        return taskService.createTask(task);
     }
 }
