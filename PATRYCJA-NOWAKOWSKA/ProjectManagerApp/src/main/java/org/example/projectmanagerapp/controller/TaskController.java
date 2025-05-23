7e147043c9008d112ff0ba541c8afa5ba70ed35a
Patrycja <patrycja.nowakowska3@gmail.com>
2025-05-23 17:02:11 +0200

    dodanie kontrolerów dla Task i Users

--- PATRYCJA-NOWAKOWSKA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/TaskController.java
+++ PATRYCJA-NOWAKOWSKA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/TaskController.java
@@ -1,5 +1,7 @@
 package org.example.projectmanagerapp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.projectmanagerapp.entity.Task;
 import org.example.projectmanagerapp.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,17 +11,20 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
+@Tag(name = "Tasks", description = "Operacje związane z zadaniami")
 public class TaskController {
 
     @Autowired
     private TaskRepository taskRepository;
 
     @GetMapping
+    @Operation(summary = "Pobierz listę wszystkich zadań", description = "Zwraca listę wszystkich zadań")
     public List<Task> getAllTasks() {
         return taskRepository.findAll();
     }
 
     @PostMapping
+    @Operation(summary = "Dodaj nowe zadanie", description = "Tworzy nowe zadanie")
     public Task createTask(@RequestBody Task task) {
         return taskRepository.save(task);
     }
--- PATRYCJA-NOWAKOWSKA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/UserController.java
+++ PATRYCJA-NOWAKOWSKA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/UserController.java
@@ -1,5 +1,7 @@
 package org.example.projectmanagerapp.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.example.projectmanagerapp.entity.User;
 import org.example.projectmanagerapp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,17 +11,20 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
+@Tag(name = "Users", description = "Operacje związane z użytkownikami")
 public class UserController {
 
     @Autowired
     private UserRepository userRepository;
 
     @GetMapping
+    @Operation(summary = "Pobierz wszystkich użytkowników", description = "Zwraca listę wszystkich użytkowników w systemie")
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     @PostMapping
+    @Operation(summary = "Dodaj nowego użytkownika", description = "Tworzy nowego użytkownika")
     public User createUser(@RequestBody User user) {
         return userRepository.save(user);
     }
