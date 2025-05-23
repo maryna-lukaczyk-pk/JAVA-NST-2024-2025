3a97f8d8f0d475feb719eb3eeaa32b006ad5c166
Patrycja <patrycja.nowakowska3@gmail.com>
2025-05-23 17:54:11 +0200

    Utworzono klasę serwisową oraz zmodyfikowano kontroler Project

--- PATRYCJA-NOWAKOWSKA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/ProjectController.java
+++ PATRYCJA-NOWAKOWSKA/ProjectManagerApp/src/main/java/org/example/projectmanagerapp/controller/ProjectController.java
@@ -1,9 +1,9 @@
 package org.example.projectmanagerapp.controller;
 
 import org.example.projectmanagerapp.entity.Project;
+import org.example.projectmanagerapp.service.ProjectService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.example.projectmanagerapp.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,18 +14,22 @@ import java.util.List;
 @Tag(name = "Projects", description = "Operacje na projektach")
 public class ProjectController {
 
+    private ProjectService projectService;
+
     @Autowired
-    private ProjectRepository projectRepository;
+    private ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
 
     @GetMapping
     @Operation (summary = "Pobierz wszystkie projekty", description = "Zwraca liste wszystkich zapisanych projektów")
     public List<Project> getAllProjects() {
-        return projectRepository.findAll();
+        return projectService.getAllProjects();
     }
 
     @PostMapping
     @Operation (summary = "Utwórz nowy projekt", description = "Dodaje nowy projekt")
     public Project createProject(@RequestBody Project project) {
-        return projectRepository.save(project);
+        return projectService.createProject(project);
     }
 }
