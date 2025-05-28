0da40eea2300a1edda246caaf9b9a9f4c5403ed7
Michał Kruczała <kruczalamichal@gmail.com>
2025-05-28 21:43:15 +0200

    integration test added

--- MICHAŁ-KRUCZAŁA/src/main/java/org/example/projectmanagerapp/entity/Task.java
+++ MICHAŁ-KRUCZAŁA/src/main/java/org/example/projectmanagerapp/entity/Task.java
@@ -1,38 +1,87 @@
 package org.example.projectmanagerapp.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.example.projectmanagerapp.PriorityLevel;
 
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
 @Entity
 @Table(name = "tasks")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     private String title;
+
     @Column(columnDefinition = "TEXT")
     private String description;
+
     @Enumerated(EnumType.STRING)
     @Column(name = "task_type")
     private Task_Type task_type;
+
     @ManyToOne
+    @JsonBackReference
     private Project project;
+
     @Transient
     private PriorityLevel priority;
+
+    public enum Task_Type {
+        TYPE1, TYPE2, TYPE3
+    }
+
+    public int getId() {
+        return id;
+    }
 
     public void setId(int id) {
         this.id = id;
     }
 
-    public enum Task_Type {
-        TYPE1, TYPE2, TYPE3
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Task_Type getTask_type() {
+        return task_type;
+    }
+
+    public void setTask_type(Task_Type task_type) {
+        this.task_type = task_type;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
+    }
+
+    public PriorityLevel getPriority() {
+        return priority;
+    }
+
+    public void setPriority(PriorityLevel priority) {
+        this.priority = priority;
     }
 }
--- MICHAŁ-KRUCZAŁA/src/main/java/org/example/projectmanagerapp/services/ProjectService.java
+++ MICHAŁ-KRUCZAŁA/src/main/java/org/example/projectmanagerapp/services/ProjectService.java
@@ -1,17 +1,31 @@
 package org.example.projectmanagerapp.services;
 
 import org.example.projectmanagerapp.entity.Project;
+import org.example.projectmanagerapp.entity.User;
 import org.example.projectmanagerapp.repositories.ProjectRepository;
+import org.example.projectmanagerapp.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 public class ProjectService {
 
     private final ProjectRepository projectRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    public Project assignUsersToProject(int projectId, List<Integer> userIds) {
+        Project project = projectRepository.findById(projectId).orElseThrow();
+        Set<User> users = new HashSet<>(userRepository.findAllById(userIds));
+        project.setUsers(users);
+        return projectRepository.save(project);
+
+    }
 
     @Autowired
     public ProjectService(ProjectRepository projectRepository) {
